afd1ff0afb1011cdd21e0bfcdeb681e4bc23dc91
Aviral Agrawal <64287278+Avrl-agrwl@users.noreply.github.com>
2021-04-29 19:05:27 -0400

    Update project.py

--- project.py
+++ project.py
@@ -24,9 +24,7 @@ def main():
     subject="ADMG7 Bank: Your daily dose of handpicked Products"
     try:
         if not cust_name:
-            z="customer "+usr
-            cust_name.append(z)
-            body="Hi "+cust_name[0]+",\nBased on your recent interest in our " + str(product)+" service, these are the products you may like: \n1. " + prod_list[0]+"\n2. "+ prod_list[1]+" \n3. "+ prod_list[2]+" \n4. "+ prod_list[3]
+            body="Hi "+usr+",\nBased on your recent interest in our " + str(product)+" service, these are the products you may like: \n1. " + prod_list[0]+"\n2. "+ prod_list[1]+" \n3. "+ prod_list[2]+" \n4. "+ prod_list[3]
         else:
             body="Hi "+cust_name+",\nBased on your recent interest in our " + str(product)+" service, these are the products you may like: \n1. " + prod_list[0]+"\n2. "+ prod_list[1]+" \n3. "+ prod_list[2]+" \n4. "+ prod_list[3]
     except:
